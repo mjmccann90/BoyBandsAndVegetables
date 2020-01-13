ee329df915d6f713b6248c3e3b2e06947596158c
Mark McCann <mjmccann12@me.com>
2020-01-13 09:17:51 -0600

    completed boy bands and veggies

---
+++ bbv.js
@@ -0,0 +1,57 @@
+/*
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+
+The number of loops to perform (what if the array changes?)
+var loopCount = 5;
+
+Keep track of which band we're on in the loop
+var currentBand = "";
+
+Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById(???);
+
+Get a reference to the appropriate DOM element for vegetables
+var veggieElement = ???;
+
+Start looping
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+
+    Add the band names into the correct <div>
+    currentBand = ???;
+
+
+    Add the veggie names into the correct <div>
+    currentVeggie = ???;
+
+}
+
+*/
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+
+var loopCount = 5;
+var currentBand = "";
+var currentVeggie = "";
+
+
+var bandElement = document.getElementById("boy-bands");
+var veggieElement = document.getElementById("vegetables");
+
+
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+    currentBand = bands[loopTracker];
+    bandElement.innerHTML += "<p>" + currentBand + "</p>";
+}
+
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+    currentVeggie = vegetables[loopTracker];
+    veggieElement.innerHTML += "<p>" + currentVeggie + "</p>";
+}
